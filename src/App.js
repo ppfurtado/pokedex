9442745efd24f6fb4798255148572c48a0c6fe22
ppfurtado <furtado.pedropaulo@gmail.com>
2021-03-08 23:27:39 -0300

    first commit '-'

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import styled from 'styled-components'
+import Card from './componentes/Card'
 
-function App() {
+const Wrapper = styled.div`
+  display: grid;
+  grid-template-columns: repeat(5,1fr);
+  width: 96%;
+  margin: 0 auto;
+  justify-items: center;
+  gap: 20px;
+  padding: 10px;
+`
+const Title = styled.h2`
+  text-align: center;
+`
+const App = () => {
+  const [data, setData] = React.useState(null)
+  const getPokemonUrl = id => `https://pokeapi.co/api/v2/pokemon/${id}`
+  
+  React.useEffect(()=>{
+
+    async function getData(){
+      const pokemonsPromises = []
+
+      for(let i = 1;i<= 150;i++){
+        const response = await fetch(getPokemonUrl(i))
+        const json = await response.json()
+        pokemonsPromises.push(json)
+      }
+      setData(pokemonsPromises) 
+    }
+    getData()
+  },[])
+
+  if (data === null) return null
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <Title>Pokedex</Title>
+      <Wrapper>
+        {
+          data.map((element)=> ( 
+            <Card 
+              key={element.name} 
+              name={element.name} 
+              typepokemon={element.types.map(typeInfo => typeInfo.type.name).join(' | ')}
+              type={element.types.map(typeInfo => typeInfo.type.name)} 
+              alt={element.name} url={element.sprites.other.dream_world.front_default}   
+            />
+          ))
+        }
+      </Wrapper>
+    </>
+  )
 }
 
-export default App;
+export default App
+
---
+++ src/componentes/Card.js
@@ -0,0 +1,72 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const typesBackground = {
+  steel : '#f4f4f4',
+  
+  fire : '#FDDFDF',
+  
+  grass : '#DEFDE0',
+  
+  electric : '#FCF7DE',
+  
+  water : '#DEF3FD', 
+  ice : '#DEF3FD',  
+  ground : '#f4e7da',
+  
+  rock : '#d5d5d4',
+  
+  fairy : '#fceaff',
+  
+  poison : '#98d7a5',
+  
+  bug : '#f8d5a3',
+  
+  dragon : '#97b3e6',
+  
+  psychic : '#eaeda1',
+  
+  flying : '#F5F5F5',
+  
+  fighting : '#E6E0D4',
+  
+  normal : '#F5F5F5',
+}
+
+const ContainerCard = styled.div`
+  width: 200px;
+  height: 240px;
+  display: flex;
+  flex-direction: column;
+  background: ${props => typesBackground[props.type[0]]};
+  align-items: center;
+  padding-top: 1rem;
+  border-radius: 5px;
+  justify-content: center;
+
+  &:hover {
+    transform: scale(1.1);
+  }
+`
+const NamePokemon = styled.h2`
+  margin: 10px 0px 0px 0px;
+`
+const TypePokemon = styled.h4`
+  margin: 0px;
+`
+
+const Card = ({ name, typepokemon, url, alt, type }) => {
+  return (
+    <ContainerCard type={type} >
+      <img style={{display: 'block', maxWidth: '40%'}} src={url} alt={alt} />
+      <NamePokemon>
+        {name}
+      </NamePokemon>
+      <TypePokemon>
+        {typepokemon}
+      </TypePokemon>      
+    </ContainerCard>
+  )
+}
+
+export default Card
