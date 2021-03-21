e3fbd4de415065faea5a94b43ee81f852735a291
ppfurtado <furtado.pedropaulo@gmail.com>
2021-03-21 01:04:17 -0300

    fix background Details

--- src/componentes/Details.js
+++ src/componentes/Details.js
@@ -9,22 +9,22 @@ import { PokedexContext } from '../contexts/PokedexContext'
 const Wrapper = styled.div`
   width: 80%;
   margin: 0 auto;
-`
-const ArrowLeft = styled(Arrow)`
-  position: absolute;
-  width: 30px;
-  left: 10px;
-  top: 10px;
+  background: ${props => props.typesBackground[props.type]};
 `
 const Header = styled.div`
   position: relative;
   width: 100%;
   height: 200px;
   
-  background: ${props => props.typesBackground[props.type]};
   display: flex;
   justify-content: center;
   align-items: center;
+`
+const ArrowLeft = styled(Arrow)`
+  position: absolute;
+  width: 30px;
+  left: 10px;
+  top: 10px;
 `
 const Image = styled.img.attrs( props =>({
   src: props.src,
@@ -34,6 +34,12 @@ const Image = styled.img.attrs( props =>({
   max-height: 100%;
   position: absolute;
   bottom: -30px;
+`
+const Content = styled.div`
+  width: 100%;
+  height: 400px;
+  background: white;
+  border-radius: 50px 50px 0 0;
 `
 
 const Details = () => {
@@ -46,12 +52,12 @@ const Details = () => {
   if(data === null) return null
   console.log(data[id -1 ]);
   return (
-    <Wrapper>
-      <Header typesBackground={typesBackground} type={data[id].types[0].type.name} >
+    <Wrapper typesBackground={typesBackground} type={data[id - 1].types[0].type.name}>
+      <Header >
         <ArrowLeft onClick={() => history(-1)} />
         <Image src={data[id - 1].sprites.other.dream_world.front_default} alt={data[id - 1].forms[0].name} />
-        {/* <img style={{display: 'block', maxHeight: '80%' }} src={data[id - 1].sprites.other.dream_world.front_default} alt=""/>       */}
       </Header>
+      <Content />
       
     </Wrapper>
   )
